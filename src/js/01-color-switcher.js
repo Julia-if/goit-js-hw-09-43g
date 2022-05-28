62dafe466bc4806efd4deadcd0917d56e2a89809
Julia-if <juliaif18@gmail.com>
2022-05-28 23:18:15 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
+console.log(refs.body);
 
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let timerId = null;
+
+function onBtnStartClick() {
+    timerId = setInterval(bodyChangerColor, 1000);
+    refs.btnStart.disabled = true;
+};
+
+function bodyChangerColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function onBtnStopClick() {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+}
